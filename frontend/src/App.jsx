40079d8e544ab97e391ec079048917f6e5f3a837
Ashwin Honnawarkar <ashwin.honnawarkar@gmail.com>
2025-06-15 14:24:53 -0400

    nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,51 @@
 import './App.css'
+import { useState } from 'react';
 
-function App() {
+
+const logo = {
+  name: "All Nighter",
+  imageUrl: '/logo.png',
+  imageSize: 85,
+};
+
+export default function App() {
+  const[count, setCount] = useState(0);
+
+  function handleClick(){
+    setCount(count + 1);
+  }
+
   return (
-    <div>App</div>
-  )
+    <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <img
+            className="logo"
+            src={logo.imageUrl}
+            alt={"Logo of " + logo.name}
+            style={{
+              width: logo.width,
+              height: logo.height,
+            }}
+          />
+          <h1 className="title">All Nighter</h1>
+        </div>
+        <div className="navbar-right">
+          <a href="#planner">Planner</a>
+          <a href="#flashcards">Flashcards</a>
+          <a href="#quizzes">Quizzes</a>
+          <a href="#progress">Progress</a>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default App
+
+function MyButton({count, onClick}){
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
